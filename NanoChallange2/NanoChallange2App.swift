7b7ef5c5c3d60c7e086557d7b53ba3cc487b2571
Azuar <Lazuardhiimaniahfar@gmail.com>
2024-05-22 15:15:57 +0700

    Initial Commit

---
+++ NanoChallange2/NanoChallange2App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallange2App.swift
+//  NanoChallange2
+//
+//  Created by Lazuardhi Imani Ahfar on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallange2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
